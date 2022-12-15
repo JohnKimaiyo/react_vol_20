9fcf03babbb6a06f8ce9021bb74a98f3f9166c81
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-15 15:57:03 +0300

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from "react";
 import "./App.css";
-import Greet from "./component/Greet";
-import Welcome from "./component/Welcome";
-import Hello from "./component/Hello";
-import MyNames from './component/MyNames'
-import Myname from "./component/Myname";
-import Mycount from "./component/Mycount";
-import Form from "./component/Form";
-import Users from "./component/Users";
-import Event from "./component/Event";
-import Range from "./component/Range";
-import Submit from "./component/Submit";
-import InputFocus from "./component/InputFocus";
+import Navbar from "./component/Navabar";
+
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
-      <Hello />
-      <MyNames name="John Dev" />
-      <MyNames name="John IT guy" />
-      <MyNames name="John Software Engineer" />
-      <Myname/>
-      <Mycount/>
-      <Form/>
-      <Users/>
-      <Event/>
-      <Range/>
-      <Submit/>
-      <InputFocus/>
+      <Navbar/>
     </div>
   );
 }
